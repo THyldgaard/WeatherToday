a8509251d76684d969aec705d3c2bcb75fa73dec
Tonni Hyldgaard <Hyldgaard@Tonnis-MacBook-Pro.local>
2016-07-09 22:05:18 +0200

    Initial Setup

---
+++ Weather Today/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Today
+//
+//  Created by Tonni Hyldgaard on 7/9/16.
+//  Copyright © 2016 Hyldgaard Informatics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
